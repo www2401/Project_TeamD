de30089e267f9090de6eb93704341acd5a0638eb
Lee YuJin <iloveyoojin@naver.com>
2017-07-27 14:55:11 +0900

    Upload

--- TeamD/app/src/main/java/com/example/ilove/teamd/TeamD.java
+++ TeamD/app/src/main/java/com/example/ilove/teamd/TeamD.java
@@ -1,8 +1,14 @@
 package com.example.ilove.teamd;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -18,7 +24,22 @@ public class TeamD extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     BluetoothChatFragment bt;
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    //Member object for the chat services
+    private BluetoothChatService mChatService = null;
+    //Name of the connected device
+    private String mConnectedDeviceName = null;
+    //Array adapter for the conversation thread
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bluetooth_chat, menu);
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,18 +58,63 @@ public class TeamD extends AppCompatActivity
 
         bt = new BluetoothChatFragment();
         Log.v("1", "dsdf");
-        setContentView(R.layout.activity_team_d);
         Log.v("2", "das");
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             BluetoothChatFragment fragment = new BluetoothChatFragment();
-            transaction.replace(R.id.content_team_d, fragment);
-            transaction.commit();
+            transaction.replace(R.id.content_team_d, fragment).commit();
         }
     }
+    //블루투스 채팅 핸들러 메인 부분
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            FragmentActivity activity = TeamD.this;
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            break;
+                    }
+                    break;
+                //메시지를 쓰는 부분
+                case Constants.MESSAGE_WRITE:
+                    break;
+                //메시지를 읽는 부분
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    bt.setText1(readMessage);
+                    //print the sensor data
+                    //mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    if (null != activity) {
+                        Toast.makeText(activity, "Connected to "
+                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case Constants.MESSAGE_TOAST:
+                    if (null != activity) {
+                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    };
     public boolean onPrepareOptionsMenu(Menu menu) {
         return super.onPrepareOptionsMenu(menu);
     }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -58,46 +124,91 @@ public class TeamD extends AppCompatActivity
             super.onBackPressed();
         }
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if(id==R.id.secure_connect_scan){
+            Intent serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
+        }
         //noinspection SimplifiableIfStatement
         return super.onOptionsItemSelected(item);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE_SECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, true);
+                }
+                break;
+            case REQUEST_CONNECT_DEVICE_INSECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, false);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    mChatService = new BluetoothChatService(this, mHandler);
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    Log.d("Main", "BT not enabled");
+                    Toast.makeText(this, "Bluetooth was not enabled. Leaving this APP",
+                            Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+        }
+    }
+    private void connectDevice(Intent data, boolean secure) {
+        // Get the device MAC address
+        String address = data.getExtras()
+                .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        mChatService.connect(device, secure);
     }
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        if (id == R.id.nav_map) {
-            Intent page =new Intent(TeamD.this,map.class);
-            startActivity(page);
-        } else if (id == R.id.nav_graph) {
-            Intent page =new Intent(TeamD.this,graph.class);
-            startActivity(page);
-        } else if (id == R.id.nav_account) {
-            Intent page =new Intent(TeamD.this,account.class);
-            startActivity(page);
-        } else if (id == R.id.nav_registration) {
-            Intent page =new Intent(TeamD.this,registration.class);
-            startActivity(page);
-        } else if (id == R.id.nav_logout) {
-            Toast.makeText(this, "log out", Toast.LENGTH_SHORT).show();
+        Log.v("CLicked","asd");
+        try {
+            int id = item.getItemId();
+            if (id == R.id.nav_map) {
+                Intent page = new Intent(TeamD.this, map.class);
+                startActivity(page);
+            } else if (id == R.id.nav_graph) {
+                Intent page = new Intent(TeamD.this, graph.class);
+                startActivity(page);
+            } else if (id == R.id.nav_account) {
+                Intent page = new Intent(TeamD.this, account.class);
+                startActivity(page);
+            } else if (id == R.id.nav_registration) {
+                Intent page = new Intent(TeamD.this, registration.class);
+                startActivity(page);
+            } else if (id == R.id.nav_logout) {
+                Toast.makeText(this, "log out", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_login) {
+                Intent page = new Intent(TeamD.this, login.class);
+                startActivity(page);
+            } else if (id == R.id.nav_heart) {
+                Intent page = new Intent(TeamD.this, heart.class);
+                startActivity(page);
+            }
+        }catch (Exception e){
+            Log.v("catched","sdf");
         }
-        else if(id==R.id.nav_login){
-            Intent page =new Intent(TeamD.this,login.class);
-            startActivity(page);
-        }
-        else if (id == R.id.nav_heart) {
-            Intent page =new Intent(TeamD.this,heart.class);
-            startActivity(page);
-        }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
