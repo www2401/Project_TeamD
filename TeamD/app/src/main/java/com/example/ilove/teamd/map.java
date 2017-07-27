3e2cab96a447218796df824bebeba3394b4706b4
Lee YuJin <iloveyoojin@naver.com>
2017-07-28 02:40:02 +0900

    upload

--- TeamD/app/src/main/java/com/example/ilove/teamd/map.java
+++ TeamD/app/src/main/java/com/example/ilove/teamd/map.java
@@ -11,20 +11,19 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.WindowManager;
+import android.os.Bundle;
+import android.util.*;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -40,56 +39,48 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class map extends AppCompatActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
+public class map extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
-
+        com.google.android.gms.location.LocationListener{
     private GoogleApiClient mGoogleApiClient = null;
     private GoogleMap mGoogleMap = null;
     private Marker currentMarker = null;
 
-    // Location of default, USCD
-    private static final LatLng DEFAULT_LOCATION = new LatLng(32.855, -117.23);
+    //디폴트 위치, Seoul
+    private static final LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
     private static final String TAG = "googlemap_example";
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2002;
-    private static final int UPDATE_INTERVAL_MS = 1000;  // 1sec
-    private static final int FASTEST_UPDATE_INTERVAL_MS = 1000; // 1sec
+    private static final int UPDATE_INTERVAL_MS = 1000;  // 1초
+    private static final int FASTEST_UPDATE_INTERVAL_MS = 1000; // 1초
 
     private AppCompatActivity mActivity;
     boolean askPermissionOnceAgain = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_map);
 
         mActivity = this;
 
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
     }
 
-    @Override
     protected void onStart() {
         super.onStart();
-
-        //if (mGoogleApiClient != null)
-        // mGoogleApiClient.connect();
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
-        if (mGoogleApiClient!=null)
+        if (mGoogleApiClient != null)
             mGoogleApiClient.connect();
 
-        // Recheck for permission in app information
+        //앱 정보에서 퍼미션을 허가했는지를 다시 검사해봐야 한다.
         if (askPermissionOnceAgain) {
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -112,18 +103,20 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
     @Override
     public void onPause() {
 
-        // Stop update location
+        //위치 업데이트 중지
         if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
 
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
 
             mGoogleApiClient.disconnect();
         }
+
         super.onPause();
     }
 
     @Override
     protected void onDestroy() {
+
         if (mGoogleApiClient != null) {
             mGoogleApiClient.unregisterConnectionCallbacks(this);
             mGoogleApiClient.unregisterConnectionFailedListener(this);
@@ -133,20 +126,31 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
                         .removeLocationUpdates(mGoogleApiClient, this);
                 mGoogleApiClient.disconnect();
             }
+
         }
+
         super.onDestroy();
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
 
-        Log.d(TAG, "onMapReady");
         mGoogleMap = map;
+        //내위치 말고 다른 위치에 마커 띄우기(고정된 위치)
+        // 1. 마커 옵션 설정 (만드는 과정)
+        MarkerOptions makerOptions = new MarkerOptions();
+        makerOptions
+                .position(new LatLng(34, -118))
+                .title("Different location!"); // 타이틀.
+        // 2. 마커 생성 (마커를 나타냄)
+        mGoogleMap.addMarker(makerOptions);
+        // 카메라를 위치로 옮긴다.
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(37.52487, 126.92723)));
 
-        // Before msg about runtime permission or GPS activation,
-        // move the first location to UCSD
-        setCurrentLocation(null, "Can't load location informaiton",
-                "Please check your location permission and GPS activation");
+        //런타임 퍼미션 요청 대화상자나 GPS 활성 요청 대화상자 보이기전에
+        //지도의 초기위치를 서울로 이동
+        setCurrentLocation(null, "위치정보 가져올 수 없음",
+                "위치 퍼미션과 GPS 활성 요부 확인하세요");
 
         mGoogleMap.getUiSettings().setCompassEnabled(true);
         //mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
@@ -154,14 +158,14 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
 
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // If API is over 23, need runtime permission
+            //API 23 이상이면 런타임 퍼미션 처리 필요
 
             int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION);
 
             if (hasFineLocationPermission == PackageManager.PERMISSION_DENIED) {
-                ActivityCompat.requestPermissions( mActivity,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                ActivityCompat.requestPermissions(mActivity,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                         PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
 
             } else {
@@ -195,13 +199,13 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
     @Override
     public void onLocationChanged(Location location) {
 
-        Log.d( TAG, "onLocationChanged");
+        android.util.Log.d(TAG, "onLocationChanged");
         String markerTitle = getCurrentAddress(location);
-        String markerSnippet = " Latitude:"+String.valueOf(location.getLatitude())
-                + " Longitude:"+String.valueOf(location.getLongitude());
+        String markerSnippet = "위도:" + String.valueOf(location.getLatitude())
+                + " 경도:" + String.valueOf(location.getLongitude());
 
-        // Make a marker on current location
-        setCurrentLocation(location, markerTitle, markerSnippet );
+        //현재 위치에 마커 생성
+        setCurrentLocation(location, markerTitle, markerSnippet);
     }
 
 
@@ -220,7 +224,7 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
     @Override
     public void onConnected(Bundle connectionHint) {
 
-        Log.d(TAG,"onConnected");
+        android.util.Log.d(TAG, "onConnected");
         if (!checkLocationServicesStatus()) {
             showDialogForLocationServiceSetting();
         }
@@ -240,10 +244,9 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
                         .requestLocationUpdates(mGoogleApiClient, locationRequest, this);
 
             }
-        }
-        else{
+        } else {
 
-            Log.d(TAG,"onConnected : call FusedLocationApi");
+            android.util.Log.d(TAG, "onConnected : call FusedLocationApi");
             LocationServices.FusedLocationApi
                     .requestLocationUpdates(mGoogleApiClient, locationRequest, this);
 
@@ -260,26 +263,25 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
         location.setLatitude(DEFAULT_LOCATION.latitude);
         location.setLongitude(DEFAULT_LOCATION.longitude);
 
-        setCurrentLocation(location, "Can't load location information",
-                "Please check your location permission and GPS activation");
+        setCurrentLocation(location, "위치정보 가져올 수 없음",
+                "위치 퍼미션과 GPS 활성 요부 확인하세요");
     }
-
 
 
     @Override
     public void onConnectionSuspended(int cause) {
-        if ( cause ==  CAUSE_NETWORK_LOST )
-            Log.e(TAG, "onConnectionSuspended(): Google Play services " +
+        if (cause == CAUSE_NETWORK_LOST)
+            android.util.Log.e(TAG, "onConnectionSuspended(): Google Play services " +
                     "connection lost.  Cause: network lost.");
-        else if (cause == CAUSE_SERVICE_DISCONNECTED )
-            Log.e(TAG,"onConnectionSuspended():  Google Play services " +
+        else if (cause == CAUSE_SERVICE_DISCONNECTED)
+            android.util.Log.e(TAG, "onConnectionSuspended():  Google Play services " +
                     "connection lost.  Cause: service disconnected");
     }
 
 
-    public String getCurrentAddress(Location location){
+    public String getCurrentAddress(Location location) {
 
-        // Change GEO coder to GPS address
+        //지오코더... GPS를 주소로 변환
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
 
         List<Address> addresses;
@@ -291,19 +293,19 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
                     location.getLongitude(),
                     1);
         } catch (IOException ioException) {
-            // Network problem
-            Toast.makeText(this, "Can't use GEO coder service", Toast.LENGTH_LONG).show();
-            return "Can't use GEO coder service";
+            //네트워크 문제
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용불가";
         } catch (IllegalArgumentException illegalArgumentException) {
-            Toast.makeText(this, "Wrong GPS coordinate", Toast.LENGTH_LONG).show();
-            return "Wrong GPS coordinate";
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
 
         }
 
 
         if (addresses == null || addresses.size() == 0) {
-            Toast.makeText(this, "Can't find address", Toast.LENGTH_LONG).show();
-            return "Can't find address";
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
 
         } else {
             Address address = addresses.get(0);
@@ -313,7 +315,7 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
     }
 
 
-    public boolean checkLocationServicesStatus(){
+    public boolean checkLocationServicesStatus() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
@@ -327,9 +329,9 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
 
 
         if (location != null) {
-            LatLng currentLocation = new LatLng( location.getLatitude(), location.getLongitude());
+            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
-            // Change marker image what you want
+            //마커를 원하는 이미지로 변경해줘야함
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(currentLocation);
             markerOptions.title(markerTitle);
@@ -356,7 +358,7 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
     }
 
 
-    // Method for runtime permission
+    //여기부터는 런타임 퍼미션 처리을 위한 메소드들
     @TargetApi(Build.VERSION_CODES.M)
     private void checkPermissions() {
         boolean fineLocationRationale = ActivityCompat
@@ -366,16 +368,14 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
                 Manifest.permission.ACCESS_FINE_LOCATION);
 
         if (hasFineLocationPermission == PackageManager
-                .PERMISSION_DENIED && fineLocationRationale )
-            showDialogForPermission("For execute app, authorise your permission");
+                .PERMISSION_DENIED && fineLocationRationale)
+            showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
 
         else if (hasFineLocationPermission
-                == PackageManager.PERMISSION_DENIED && !fineLocationRationale ) {
-            showDialogForPermissionSetting("Refuse permission + Don't ask again " +
-                    "Set the checkbox now, have to authorise your permission");
-        }
-
-        else if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
+                == PackageManager.PERMISSION_DENIED && !fineLocationRationale) {
+            showDialogForPermissionSetting("퍼미션 거부 + Don't ask again(다시 묻지 않음) " +
+                    "체크 박스를 설정한 경우로 설정에서 퍼미션 허가해야합니다.");
+        } else if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
 
             if (mGoogleApiClient == null) {
                 buildGoogleApiClient();
@@ -411,8 +411,7 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
                 }
 
 
-            }
-            else {
+            } else {
 
                 checkPermissions();
             }
@@ -424,18 +423,18 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
     private void showDialogForPermission(String msg) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(map.this);
-        builder.setTitle("Al");
+        builder.setTitle("알림");
         builder.setMessage(msg);
         builder.setCancelable(false);
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                ActivityCompat.requestPermissions( mActivity,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                ActivityCompat.requestPermissions(mActivity,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                         PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
             }
         });
 
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 finish();
             }
@@ -446,10 +445,10 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
     private void showDialogForPermissionSetting(String msg) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(map.this);
-        builder.setTitle("Alert");
+        builder.setTitle("알림");
         builder.setMessage(msg);
         builder.setCancelable(true);
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
 
                 askPermissionOnceAgain = true;
@@ -461,7 +460,7 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
                 mActivity.startActivity(myAppSettings);
             }
         });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 finish();
             }
@@ -470,31 +469,30 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
     }
 
 
-    // Method for GPS activation
+    //여기부터는 GPS 활성화를 위한 메소드들
     private void showDialogForLocationServiceSetting() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(map.this);
-        builder.setTitle("Not activate location service");
-        builder.setMessage("For use app, need location sevice.\n"
-                + "Correct the location setting?" );
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                + "위치 설정을 수정하실래요?");
         builder.setCancelable(true);
-        builder.setPositiveButton("Setting", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int id) {
                 Intent callGPSSettingIntent
-                        = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
             }
         });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int id){
+            public void onClick(DialogInterface dialog, int id) {
                 dialog.cancel();
             }
         });
         builder.create().show();
     }
-
 
 
     @Override
@@ -505,7 +503,7 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
 
             case GPS_ENABLE_REQUEST_CODE:
 
-                // Check for GPS activation
+                //사용자가 GPS 활성 시켰는지 검사
                 if (checkLocationServicesStatus()) {
                     if (checkLocationServicesStatus()) {
 
@@ -519,19 +517,16 @@ public class map extends AppCompatActivity implements OnMapReadyCallback,
 
                                 mGoogleMap.setMyLocationEnabled(true);
                             }
-                        }
-                        else  mGoogleMap.setMyLocationEnabled(true);
+                        } else mGoogleMap.setMyLocationEnabled(true);
 
                         return;
                     }
-                }
-                else{
-                    setCurrentLocation(null, "Can't take location information.",
-                            "Please check your location permission and GPS activation.");
+                } else {
+                    setCurrentLocation(null, "위치정보 가져올 수 없음",
+                            "위치 퍼미션과 GPS 활성 요부 확인하세요");
                 }
 
                 break;
         }
     }
 }
-
