1972d91224b1159c4fc43265e9d2a1b6929aff1a
Lee YuJin <iloveyoojin@naver.com>
2017-08-11 20:11:15 +0900

    up

--- TeamD/app/src/main/java/com/example/ilove/teamd/BluetoothChatFragment.java
+++ TeamD/app/src/main/java/com/example/ilove/teamd/BluetoothChatFragment.java
@@ -56,6 +56,8 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static com.example.ilove.teamd.current.Mstatus;
+
 
 /**
  * This fragment controls Bluetooth to communicate with other devices.
@@ -70,6 +72,10 @@ public class BluetoothChatFragment extends Fragment {
     public ArrayList<Entry> so2value = new ArrayList<Entry>();
     public ArrayList<Entry> o3value = new ArrayList<Entry>();
     public ArrayList<Entry> pm25value = new ArrayList<Entry>();
+    public float CO_AQI, NO2_AQI, SO2_AQI, O3_AQI, PM25_AQI = 0;
+    public float CO, NO2, SO2, O3, PM25, TEM  = 0;
+    public float co_avg, no2_avg, so2_avg, o3_avg, pm25_avg = 0;
+    public float co_old, no2_old, so2_old, o3_old, pm25_old = 0;
 
     View view;
 
@@ -89,8 +95,6 @@ public class BluetoothChatFragment extends Fragment {
     static public TextView so2_air;
     static public TextView no2_air;
     static public TextView pm25_air;
-
-    int CO, NO2, SO2, O3, PM25, TEM  = 0;
 
     int count = -1;
 
@@ -125,11 +129,16 @@ public class BluetoothChatFragment extends Fragment {
     /**
      * Member object for the chat services
      */
-
+    public static ArrayList<Float> co_bufferArrayList = new ArrayList<Float>();
+    public static ArrayList<Float> no2_bufferArrayList = new ArrayList<Float>();
+    public static ArrayList<Float> so2_bufferArrayList = new ArrayList<Float>();
+    public static ArrayList<Float> o3_bufferArrayList = new ArrayList<Float>();
+    public static ArrayList<Float> pm25_bufferArrayList = new ArrayList<Float>();
     private Handler mTimerHandler = new Handler();
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         // Get local Bluetooth adapter
@@ -415,20 +424,34 @@ public class BluetoothChatFragment extends Fragment {
 
                         try {
 
+                            count++;
                             JSONObject JsonAir = new JSONObject(startMessage);
 
+                            Mstatus = tempValue.Mstatus_tv;
+                            if(Mstatus == 1){current.setCircleColorButton(tempValue.CO_AQI_tv); }
+                            if(Mstatus == 2){current.setCircleColorButton(tempValue.SO2_AQI_tv); }
+                            if(Mstatus == 3){current.comfirmsetCircleColorButton(tempValue.NO2_tv); } //이것만 로우데이터로 확인중 나머지는 AQI
+                            if(Mstatus == 4){current.setCircleColorButton(tempValue.O3_AQI_tv); }
+                            if(Mstatus == 5){current.setCircleColorButton(tempValue.PM25_AQI_tv); }
+                            //Toast.makeText(activity, readMessage, Toast.LENGTH_SHORT).show();
                             CO = JsonAir.getInt("CO");
                             NO2 = JsonAir.getInt("NO2");
                             SO2 = JsonAir.getInt("SO2");
                             O3 = JsonAir.getInt("O3");
                             PM25 = JsonAir.getInt("PM25");
+                            co_bufferArrayList.add(CO);
+                            no2_bufferArrayList.add(NO2);
+                            so2_bufferArrayList.add(SO2);
+                            o3_bufferArrayList.add(O3);
+                            pm25_bufferArrayList.add(PM25);
 
-                            float co_aqi = calcurate_co_aqi(CO);
-                            float no2_aqi = calcurate_no2_aqi(NO2);
-                            float so2_aqi = calcurate_so2_aqi(SO2);
-                            float o3_aqi = calcurate_o3_aqi(O3);
-                            float pm25_aqi = calcurate_pm25_aqi(PM25);
-/*
+
+                            CO_AQI = calcurate_co_aqi(calcurate_co_avg());
+                            SO2_AQI = calcurate_so2_aqi(calcurate_so2_avg());
+                            NO2_AQI = calcurate_no2_aqi(calcurate_no2_avg());
+                            O3_AQI = calcurate_o3_aqi(calcurate_o3_avg());
+                            PM25_AQI = calcurate_pm25_aqi(calcurate_pm25_avg());
+
                             tempValue.CO_AQI_tv = CO_AQI;//값넘기는거
                             tempValue.SO2_AQI_tv = SO2_AQI;
                             tempValue.NO2_AQI_tv = NO2_AQI;
@@ -438,14 +461,14 @@ public class BluetoothChatFragment extends Fragment {
                             tempValue.SO2_tv = SO2;
                             tempValue.NO2_tv = NO2;
                             tempValue.O3_tv = O3;
-                            tempValue.PM25_tv = PM25;*/
+                            tempValue.PM25_tv = PM25;
 
                             temp.setText(JsonAir.getString("temp"));
-                            co_air.setText(String.valueOf(co_aqi));   //toString 이 뭔가를 String으로 바꿔주는거
-                            o3_air.setText(String.valueOf(o3_aqi));
-                            so2_air.setText(String.valueOf(so2_aqi));   //toString 이 뭔가를 String으로 바꿔주는거
-                            no2_air.setText(String.valueOf(no2_aqi));
-                            pm25_air.setText(String.valueOf(pm25_aqi));
+                            co_air.setText(String.valueOf(CO_AQI));   //toString 이 뭔가를 String으로 바꿔주는거
+                            o3_air.setText(String.valueOf(SO2_AQI));
+                            so2_air.setText(String.valueOf(NO2_AQI));   //toString 이 뭔가를 String으로 바꿔주는거
+                            no2_air.setText(String.valueOf(O3_AQI));
+                            pm25_air.setText(String.valueOf(PM25_AQI));
 
 
                             JsonTransfer airdata_transfer = new JsonTransfer();
@@ -481,7 +504,7 @@ public class BluetoothChatFragment extends Fragment {
 
                             // o3_one_min.toString(); 어레이 전체 값을 스트링으로 변환 해서 토스트 값 확인 할 수 있음
 
-                            aqi((int)co_aqi,(int)no2_aqi,(int)so2_aqi,(int)o3_aqi,(int)pm25_aqi);
+                            aqi((int)CO_AQI,(int)SO2_AQI,(int)NO2_AQI,(int)O3_AQI,(int)PM25_AQI);
                             setData();
 
                             //json_dataTransfer의 데이터들을 하나의 json_string으로 묶는다.
@@ -998,6 +1021,143 @@ public class BluetoothChatFragment extends Fragment {
 
         pm_Aqi = ((I_hight-I_low)/(C_hight-C_low))*(C-C_low)+I_low;
         return pm_Aqi;
+    }
+    public float calcurate_co_avg(){
+        float co_sum=0;
+        if(count > 9600){
+            co_avg = (co_avg * 9600 + co_bufferArrayList.get(count) - co_old)/9600;
+            co_bufferArrayList.remove(0);
+            co_old = co_bufferArrayList.get(0);
+        }
+        else if(count == 9600){
+            co_avg = CO;
+            co_old = co_bufferArrayList.get(0);
+        }
+        else {
+            for(int i = 0; i < co_bufferArrayList.size(); i++) {
+                co_sum += co_bufferArrayList.get(i);
+            }
+            co_avg = co_sum / co_bufferArrayList.size();
+        }
+        return co_avg;
+    }
+
+    public float calcurate_no2_avg(){
+        float no2_sum=0;
+        if(count > 1200){
+            no2_avg = (no2_avg * 1200 + no2_bufferArrayList.get(count) - no2_old)/1200;
+            no2_bufferArrayList.remove(0);
+            no2_old = no2_bufferArrayList.get(0);
+        }
+        else if(count == 1200){
+            no2_avg = NO2;
+            no2_old = no2_bufferArrayList.get(0);
+        }
+        else {
+            for(int i = 0; i < no2_bufferArrayList.size(); i++) {
+                no2_sum += no2_bufferArrayList.get(i);
+            }
+            no2_avg = no2_sum / no2_bufferArrayList.size();
+        }
+        return no2_avg;
+    }
+
+    public float calcurate_so2_avg(){
+        float so2_sum=0;
+        if(SO2 >= 0 && SO2 <=304){
+            if(count > 1200){
+                so2_avg = (so2_avg * 1200 + so2_bufferArrayList.get(count) - so2_old)/1200;
+                so2_bufferArrayList.remove(0);
+                so2_old = so2_bufferArrayList.get(0);
+            }
+            else if(count == 1200){
+                so2_avg = SO2;
+                so2_old = so2_bufferArrayList.get(0);
+            }
+            else {
+                for(int i = 0; i < so2_bufferArrayList.size(); i++) {
+                    so2_sum += so2_bufferArrayList.get(i);
+                }
+                so2_avg = so2_sum / so2_bufferArrayList.size();
+            }
+        }
+        else if(SO2 >= 305 && SO2 <= 1004){
+            if(count > 28800){
+                so2_avg = (so2_avg * 28800 + so2_bufferArrayList.get(count) - so2_old)/28800;
+                so2_bufferArrayList.remove(0);
+                so2_old = so2_bufferArrayList.get(0);
+            }
+            else if(count == 28800){
+                so2_avg = SO2;
+                so2_avg = so2_bufferArrayList.get(0);
+            }
+            else {
+                for(int i = 0; i < so2_bufferArrayList.size(); i++) {
+                    so2_sum += so2_bufferArrayList.get(i);
+                }
+                so2_avg = so2_sum / so2_bufferArrayList.size();
+            }
+        }
+        return so2_avg;
+    }
+
+    public float calcurate_o3_avg(){
+        float o3_sum=0;
+        if(O3 >= 0 && O3 <=200){
+            if(count > 9600){
+                o3_avg = (o3_avg * 9600 + o3_bufferArrayList.get(count) - o3_old)/9600;
+                o3_bufferArrayList.remove(0);
+                o3_old = o3_bufferArrayList.get(0);
+            }
+            else if(count == 9600){
+                o3_avg = O3;
+                o3_old = o3_bufferArrayList.get(0);
+            }
+            else {
+                for(int i = 0; i < o3_bufferArrayList.size(); i++) {
+                    o3_sum += o3_bufferArrayList.get(i);
+                }
+                o3_avg = o3_sum / o3_bufferArrayList.size();
+            }
+        }
+        else if(O3 >= 201 && O3 <= 604){
+            if(count > 1200){
+                o3_avg = (o3_avg * 1200 + o3_bufferArrayList.get(count) - o3_old)/1200;
+                o3_bufferArrayList.remove(0);
+                o3_old = o3_bufferArrayList.get(0);
+            }
+            else if(count == 1200){
+                o3_avg = O3;
+                o3_avg = o3_bufferArrayList.get(0);
+            }
+            else {
+                for(int i = 0; i < o3_bufferArrayList.size(); i++) {
+                    o3_sum += o3_bufferArrayList.get(i);
+                }
+                o3_avg = o3_sum / o3_bufferArrayList.size();
+            }
+        }
+        return o3_avg;
+    }
+
+    public float calcurate_pm25_avg(){
+        float pm25_sum=0;
+        if(count > 28800){
+            pm25_avg = (pm25_avg * 28800 + pm25_bufferArrayList.get(count) - pm25_old)/28800;
+            pm25_bufferArrayList.remove(0);
+            pm25_old = pm25_bufferArrayList.get(0);
+        }
+        else if(count == 28800){
+            pm25_avg = PM25;
+            pm25_old = pm25_bufferArrayList.get(0);
+        }
+        else {
+            for(int i = 0; i < pm25_bufferArrayList.size(); i++) {
+                pm25_sum += pm25_bufferArrayList.get(i);
+            }
+            pm25_avg = pm25_sum / pm25_bufferArrayList.size();
+        }
+        return pm25_avg;
     }
 
 }
