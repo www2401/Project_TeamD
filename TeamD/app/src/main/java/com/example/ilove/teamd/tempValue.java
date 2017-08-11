1b3be0267beb915f597e1dac720d824d6a20d5ae
www2401 <www2401@gmail.com>
2017-08-10 17:17:54 -0700

    up

---
+++ TeamD/app/src/main/java/com/example/ilove/teamd/tempValue.java
@@ -0,0 +1,19 @@
+package com.example.ilove.teamd;
+
+
+
+public class tempValue {
+    public static float CO_AQI_tv;
+    public static float NO2_AQI_tv;
+    public static float SO2_AQI_tv;
+    public static float O3_AQI_tv;
+    public static float PM25_AQI_tv;
+
+    public static float CO_tv;
+    public static float NO2_tv;
+    public static float SO2_tv;
+    public static float O3_tv;
+    public static float PM25_tv;
+
+    public static int Mstatus_tv;
+}
